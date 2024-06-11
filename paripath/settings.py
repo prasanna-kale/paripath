9676cf8691efc8a25bf9be3e35c0c94fbd0d2f7e
prasanna-kale <kaleprasanna405@gmail.com>
2024-06-11 21:47:12 +0530

    ok

--- paripath/settings.py
+++ paripath/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z$cuyd_2vu)$2a2^adgsnynos(5p+#y*y_k7(bpdgap$pw9rb9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
