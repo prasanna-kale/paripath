a0b35195e175696f89a3b98cced130a5fb412100
prasanna-kale <kaleprasanna405@gmail.com>
2024-06-11 18:17:23 +0530

    first commit

---
+++ paripath/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for paripath project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paripath.settings')
+
+application = get_wsgi_application()
