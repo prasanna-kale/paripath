9d564444fb3f2a8db6a9c595bcc827a3b763f161
prasanna-kale <kaleprasanna405@gmail.com>
2024-07-13 13:11:48 +0530

    ok

--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
    return render(request, 'html/index.html')
+def test(request):
+   return render(request, 'html/test.html')
--- paripath/urls.py
+++ paripath/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index , name='index'),
+    path('test/', views.test),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
